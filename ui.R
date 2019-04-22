cd8dc844dba61873fcd716885449a80b656671ef
MADELEINE DAEPP <mdaepp@gmail.com>
2019-04-22 00:54:17 -0400

    fixed missing parentheses

--- ui.R
+++ ui.R
@@ -1,36 +1,32 @@
 shinyUI(navbarPage("HNEF", id="nav",
+                   tabPanel("Interactive map",
+                            div(class="outer",
 
-tabPanel("Interactive map",
-div(class="outer",
+                #tags$head(
+                #        # Include our custom CSS
+                #        includeCSS("styles.css"),
+                #        includeScript("gomap.js")
+                #),
 
-#tags$head(
-#        # Include our custom CSS
-#        includeCSS("styles.css"),
-#        includeScript("gomap.js")
-#),
+        # If not using custom CSS, set height of leafletOutput to a number instead of percent
+        # leafletOutput("map", width="100%", height="100%"),
+        leafletOutput("map", width = 1200, height = 800),
 
-# If not using custom CSS, set height of leafletOutput to a number instead of percent
-# leafletOutput("map", width="100%", height="100%"),
-leafletOutput("map", width = 1200, height = 800),
+        # Shiny versions prior to 0.11 should use class = "modal" instead.
+        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+        draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+        width = 330, height = "auto",
 
-# Shiny versions prior to 0.11 should use class = "modal" instead.
-absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-width = 330, height = "auto",
+        h2(" "),
 
-h2(" "),
+        selectInput(inputId = "nbd",
+        label = "Select town or neighborhood:",
+        choices = levels(as.factor(ccp.dat$origin)),
+        selected = NULL, multiple = FALSE),
 
-selectInput(inputId = "nbd",
-label = "Select town or neighborhood:",
-choices = levels(as.factor(ccp.dat$origin)),
-selected = NULL, multiple = FALSE),
+        tableOutput("destinations")
+        ),
 
-tableOutput("destinations")
-),
-
-tags$div(id="cite",
-'Results constructed from the Federal Reserve Bank of New York/Equifax
-Consumer Credit Panel.')
-)
-)
-)
+        tags$div(id="cite",
+        'Results constructed from the ABC.')
+        ))))
