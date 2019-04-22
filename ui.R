81ad502cf615928d6178ea97d7f54dd13e60e4e2
MADELEINE DAEPP <mdaepp@gmail.com>
2019-04-22 00:57:41 -0400

    css only

--- ui.R
+++ ui.R
@@ -4,13 +4,13 @@ shinyUI(navbarPage("HNEF", id="nav",
 
                 tags$head(
                         # Include our custom CSS
-                        includeCSS("www/styles.css"),
-                        includeScript("www/gomap.js")
+                        includeCSS("www/styles.css")#,
+                        #includeScript("www/gomap.js")
                 ),
 
         # If not using custom CSS, set height of leafletOutput to a number instead of percent
-        # leafletOutput("map", width="100%", height="100%"),
-        leafletOutput("map", width = 1200, height = 800),
+        leafletOutput("map", width="100%", height="100%"),
+        #leafletOutput("map", width = 1200, height = 800),
 
         # Shiny versions prior to 0.11 should use class = "modal" instead.
         absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
