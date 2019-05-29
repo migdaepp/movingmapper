ef0dfd8871c41eceac09b6655abe7ee66bea638e
MADELEINE DAEPP <mdaepp@gmail.com>
2019-05-29 19:13:14 -0400

    Clear map before new selection

--- global.R
+++ global.R
@@ -15,13 +15,22 @@ library(RColorBrewer)
 
 # load the shapefile
 hns.merged <- st_read("data/hns_finalGeogs_merged.shp") %>%
-st_as_sf() %>%
-st_transform(4326) %>%
-# clean up names
-mutate(FnlGg_m = gsub("Back Bay, Downtown, Beacon Hill, West &amp; North End", "Downtown", FnlGg_m)) %>%
-mutate(FnlGg_m = gsub("_", " ", FnlGg_m))
+        st_as_sf() %>%
+        st_transform(4326) %>%
+        # clean up names
+        mutate(FnlGg_m = gsub("Back Bay, Downtown, Beacon Hill, West &amp; North End", "Downtown", FnlGg_m)) %>%
+        mutate(FnlGg_m = gsub("_", " ", FnlGg_m)) 
+hns.merged$FnlGg_m2 <- paste(hns.merged$FnlGg_m, "2", sep = "")
+hns.merged$FnlGg_m3 <- paste(hns.merged$FnlGg_m, "3", sep = "")
+hns.merged$FnlGg_m4 <- paste(hns.merged$FnlGg_m, "4", sep = "")
+
+
 
 ccp.dat <- read.csv("data/flows.min50.csv") %>%
+        # drop missing or same-same 
+        mutate(flows = ifelse(origin==destination, NA, flows)) %>%
+        #filter(origin!=destination) %>%
+        filter(origin!="" & destination!="") %>%
         # convert to average annual estimates
         mutate(flows = round((flows/15)*20,0)) %>%
         # clean up names
@@ -30,4 +39,8 @@ ccp.dat <- read.csv("data/flows.min50.csv") %>%
         mutate(origin = gsub("_", " ", origin),
                destination  = gsub("_", " ", destination))
         # create a categorical variable including "suppressed" as a category
+
+
+
+
 
--- server.R
+++ server.R
@@ -1,27 +1,19 @@
 shinyServer(function(input, output) {
     
     # set up the dataset
+        #### not sure about this -- do i use it? ####
     data <- reactive({
             if(input$whichMap=="d"){
                     ccp.dat %>%
-                            filter(destination==input$nbd) %>%
-                            mutate(flows = round(flows, 0))     
+                            filter(destination==input$nbd)    
             }else{
                     ccp.dat %>%
-                            filter(origin==input$nbd) %>%
-                            mutate(flows = round(flows, 0))
+                            filter(origin==input$nbd) 
                     
             }
-                    
-        
-        #filter(year %in% input$years[1]:input$years[2]) %>%
-        #group_by(nbd.origin, nbd.destination) %>%
-        #summarise(count = sum(count)) %>%
-        #filter(flows > 0)
-        # maybe also filter either to top ten or just those with count > 0?
     })
     
-    # add the map data
+    # create the tiles of destinations and origins
     datmap <- reactive({
             if(input$whichMap=="d"){
                     hns.merged %>%
@@ -34,7 +26,7 @@ shinyServer(function(input, output) {
             }
     })
     
-    # create the table
+    # create the table of destinations
     output$destinations <- renderTable({
         dests <- ccp.dat  %>%
                 mutate(flows = round(flows, 0)) %>%
@@ -47,6 +39,7 @@ shinyServer(function(input, output) {
         dests[1:5,]
     })
     
+    # create a table of origins
     output$origins <- renderTable({
             dests <- ccp.dat  %>%
                     mutate(flows = round(flows, 0)) %>%
@@ -60,42 +53,136 @@ shinyServer(function(input, output) {
     })
     
     # create the map
+    
     output$map <- renderLeaflet({
-        leaflet(datmap()) %>% addTiles(group = "OSM (default)") %>%
-        addTiles(group = "OSM (default)") %>%
-        addPolygons(layerId = ~FnlGg_m,
-        color = ~colorFactor(c("green", "#444444"), is.selected)(is.selected),
-        weight = 1, smoothFactor = 0.5,
-        opacity = 1.0, fillOpacity = 0.5,
-        fillColor = ~colorNumeric("YlOrRd", flows)(flows),
-        highlightOptions = highlightOptions(color = "white",
-        weight = 2,bringToFront = TRUE)) %>%
-        addLegend("bottomright", pal = colorNumeric(
-        palette = "YlOrRd",
-        domain = datmap()$flows),
-        values = ~flows, bins = 5,
-        title = "Number of Movers",
-        opacity = 1
-        ) #%>%
-        #fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+            leaflet(hns.merged) %>%
+                    addTiles(urlTemplate = "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", 
+                             attribution = 'Maps by Google') %>%
+                    addPolygons(stroke = TRUE, col = "black", weight = 0.3, 
+                                layerId = ~FnlGg_m2, 
+                                fillColor = "grey") %>%
+                    setView(lng = -71.0, lat = 42.3929, zoom = 8)
     })
+    
+#    # A reactive expression that returns the set of zips that are
+#    # in bounds right now
+#    placesInBounds <- reactive({
+#            if (is.null(input$map_bounds))
+#                    return(datmap()[FALSE,])
+#            bounds <- input$map_bounds
+#            latRng <- range(bounds$north, bounds$south)
+#            lngRng <- range(bounds$east, bounds$west)
+#            
+#            st_intersection(datmap(), 
+#                            st_as_sfc(st_bbox(c(xmin = lngRng[1], xmax = lngRng[2], 
+#                                                ymin = latRng[1], ymax = latRng[2]), 
+#                                              crs=st_crs(datmap()))))
+#    })
+    
+    #    # create the map
+    #    output$map <- renderLeaflet({
+    #        leaflet(datmap()) %>% addTiles(group = "OSM (default)") %>%
+    #        addTiles(group = "OSM (default)") %>%
+    #        addPolygons(layerId = ~FnlGg_m,
+    #        color = ~colorFactor(c("green", "#444444"), is.selected)(is.selected),
+    #        weight = 1, smoothFactor = 0.5,
+    #        opacity = 1.0, fillOpacity = 0.5,
+    #        fillColor = ~colorNumeric("YlOrRd", flows)(flows),
+    #        highlightOptions = highlightOptions(color = "white",
+    #        weight = 2,bringToFront = TRUE)) %>%
+    #        addLegend("bottomright", pal = colorNumeric(
+    #        palette = "YlOrRd",
+    #        domain = datmap()$flows),
+    #        values = ~flows, bins = 5,x
+    #        title = "Number of Movers",
+    #        opacity = 1
+    #        ) #%>%
+    #        #fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+    #    })
+    
+    # This observer is responsible for showing the flows
+    # according to the destination or origin the person has chosen
+    observe({
+            
+            leafletProxy("map", data = datmap()[datmap()$FnlGg_m!=input$nbd,]) %>%
+                    #removeShape(~FnlGg_m) %>%
+                    #removeShape("migrationFlows") %>%
+                    clearControls() %>%
+                    clearGroup("main") %>%
+                    addPolygons(layerId = ~FnlGg_m,
+                                        stroke = TRUE, col = "black", weight = 0.3,
+                            #color = ~colorFactor(c("black", "green"), is.selected)(is.selected),
+                            #weight = 1, smoothFactor = 0.5,
+                            opacity = 1.0, fillOpacity = 0.5,
+                            fillColor = ~colorNumeric("YlOrRd", flows)(flows),
+                            highlightOptions = highlightOptions(color = "white",
+                            weight = 2, bringToFront = TRUE),
+                            group = "main") %>%
+                      addLegend("bottomright", pal = colorNumeric( palette = "YlOrRd",
+                                                                   domain = datmap()$flows),
+                                values = ~flows, bins = 5,
+                                title = "Number of Movers",
+                                opacity = 1 
+                    ) 
+                    #fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+    })
+   
+    
+    observe({
+            leafletProxy("map", data = hns.merged[!hns.merged$FnlGg_m %in% datmap()$FnlGg_m &
+                                                          hns.merged$FnlGg_m!=input$nbd,]) %>%
+                    #clearGroup("main") %>%
+                    addPolygons(stroke = TRUE, col = "black", weight = 0.3, 
+                                layerId = ~FnlGg_m3, fillColor = "grey",
+                                group = "main") 
+            #                            highlightOptions = highlightOptions(color = "white",
+            #                                                                weight = 2,bringToFront = TRUE)) 
+            #        #fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+    })
+    
+    observe({
+            leafletProxy("map", data = datmap()[datmap()$FnlGg_m==input$nbd,]) %>%
+                    clearGroup("source") %>%
+                    addPolygons(layerId = ~FnlGg_m4,
+                                #stroke = FALSE,
+                                #color = ~colorFactor(c("black", "green"), is.selected)(is.selected),
+                                weight = 6, #smoothFactor = 0.5,
+                                opacity = 1.0, fillOpacity = 0.5,
+                                color = "cyan",
+                                fillColor = "grey",
+                                group = "source")
+            #                            highlightOptions = highlightOptions(color = "white",
+            #                                                                weight = 2,bringToFront = TRUE)) 
+            #        #fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+    })
+    
+
     
     # Show a popup at the given location
     showZipcodePopup <- function(place, lat, lng){
         # set up variables
         selectedNbd <- ccp.dat[ccp.dat$destination == place,][1,]
-        as.origin <- ccp.dat[ccp.dat$destination == place & ccp.dat$origin == input$nbd,] %>% mutate(flows = round(flows, 0))
-        as.destination <- ccp.dat[ccp.dat$destination == input$nbd & ccp.dat$origin == place,] %>% mutate(flows = round(flows, 0))
+        as.origin <- ccp.dat$flows[ccp.dat$destination == place & ccp.dat$origin == input$nbd]
+        as.destination <- ccp.dat$flows[ccp.dat$destination == input$nbd & ccp.dat$origin == place]
+        
+        n.inmovers <- ifelse(length(as.origin)==0, "None", 
+                            ifelse(is.na(as.origin), "Fewer than 67", 
+                            paste(format(as.integer(as.origin), big.mark = ","))))
+        
+        n.outmovers <- ifelse(length(as.destination)==0, "None", 
+                              ifelse(is.na(as.destination), "Fewer than 67", 
+                                    paste(format(as.integer(as.destination), big.mark = ","))))
+        
         # make pop-up
         content <- as.character(tagList(
         tags$h4(HTML(sprintf("%s", selectedNbd$destination))),
-        "Population in 2000:", format(as.integer(selectedNbd$population), big.mark = ","),
+        "Population in 2000:", format(as.integer(selectedNbd$pop.destination), big.mark = ","),
         tags$br(),
-        "In-movers from", sprintf("%s", paste(input$nbd, ":", sep = "")),
-        format(as.integer(as.origin$flows), big.mark = ","),
+        "In-movers from", sprintf("%s", paste(input$nbd, ": ", n.inmovers, "*", sep = "")),
         tags$br(),
-        "Out-movers to", sprintf("%s", input$nbd),":", 
-                    format(as.integer(as.destination$flows), big.mark = ","),
+        "Out-movers to", sprintf("%s", paste(input$nbd, ": ", n.outmovers, "*", sep = "")),
+        tags$br(), tags$br(),
+        "*Figures are estimated annual averages, 2003 - 2018.", 
         tags$br()
         ))
         leafletProxy("map") %>% addPopups(lng, lat, content) #content, layerId = FnlGg_m)
@@ -111,6 +198,7 @@ shinyServer(function(input, output) {
         
         isolate({
             selectedNbd <- ccp.dat[ccp.dat$destination == event$id,][1,]
+            print(event$id)
             print(selectedNbd)
             showZipcodePopup(event$id, event$lat, event$lng)
         })
